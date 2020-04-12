7ef98a5ee3acefd340ee5b58d9ea93c410c892f0
fpopescu <popescu01@gmail.com>
2020-04-11 21:54:22 -0500

    PracticalActivity_Lesson08_FP
    PracticalActivity_Lesson08_FP

--- model.py
+++ model.py
@@ -1,12 +1,19 @@
+""" script model.py """
+
+# imports
 import os
 
-from peewee import Model, CharField, IntegerField
+from peewee import Model, CharField
 from playhouse.db_url import connect
 
-db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
+# database connect
+DB = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
+
 
 class Message(Model):
+    """ mesage class """
     content = CharField(max_length=1024, unique=True)
 
     class Meta:
-        database = db
+        """ mata class """
+        database = DB
